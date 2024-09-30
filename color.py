c7da4cc37e117469ce7f93338502caa1b6c3a182
Dmiitryxx <dima.mihailovs324@gmail.com>
2024-09-30 09:18:08 +0300

    Create color.py

---
+++ color.py
@@ -0,0 +1,19 @@
+'''from termcolor import colored
+text = colored('Hello, word!', 'red', attrs=['bord'])
+print(text)
+import wikipedia
+wikipedia.set_lang("lv")
+print(w)
+from prettytable import PrettyTable
+table=PrettyTable()
+table.field_names=['Name','Age','City']
+table.add_row(['Tom','19','Liepaja'])
+print(table)'''
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('Funkcijas grafiks')
+plt.show()
+
+plt.plot([4,7,2,16,3])
+plt.ylabel('Funkcija')
+plt.show()
